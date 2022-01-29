91f408fcc2a853d0dedbe71a3606b5a9683cd4f9
Lozarth <46830521+MonkoTubeYT@users.noreply.github.com>
2022-01-29 15:48:21 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js-selfbot');
+const client = new Discord.Client();
+const config = require('./config.json');
+ 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+function bump() {
+  client.channels.cache.get(config.bumpChannel).send('!d bump');
+};
+
+(function loop() {
+  var randomNum = Math.round(Math.random() * (9000000 - 7200000  + 1)) + 7200000;
+  setTimeout(function() {
+    bump();
+    loop();  
+  }, randomNum);
+}());
+ 
+client.login(config.token);
