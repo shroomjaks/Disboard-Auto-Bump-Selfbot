66718748c77e7e7b5b236b55bf73ace808f2667e
Lozarth <46830521+Lozarth@users.noreply.github.com>
2022-12-11 19:15:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,21 +5,25 @@ require('dotenv').config()
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
-})
 
-async function bump() {
     const channel = await client.channels.fetch(process.env.bumpChannel)
-    await channel.sendSlash('302050872383242240', 'bump')
-    console.count('Bumped!')
-}
+    
+    async function bump() {
+        await channel.sendSlash('302050872383242240', 'bump')
+        console.count('Bumped!')
+    }
 
-(function loop() {
-    // send bump message every 2-3 hours, to prevent detection.
-    var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000
-    setTimeout(function () {
-        bump()
-        loop()
-    }, randomNum)
-}())
+    (function loop() {
+        // send bump message every 2-3 hours, to prevent detection.
+        var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000
+        setTimeout(function () {
+            bump()
+            loop()
+        }, randomNum)
+    }())
+    
+    bump()
+    loop()
+})
 
 client.login(process.env.token)
