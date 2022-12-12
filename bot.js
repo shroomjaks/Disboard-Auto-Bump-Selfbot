8ba319d7c23d9c25fcb4c0eec4e24cebc7672c04
Lozarth <46830521+Lozarth@users.noreply.github.com>
2022-12-11 19:05:02 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,25 @@
-const Discord = require('discord.js-selfbot');
-const client = new Discord.Client();
-const config = require('./config.json');
- 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+const { Client } = require('discord.js-selfbot-v13')
+const client = new Client()
 
-function bump() {
-  client.channels.cache.get(config.bumpChannel).send('!d bump');
-  console.count('Bumped!');
-};
+const config = require('./config.json')
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+async function bump() {
+    const channel = await client.channels.fetch(config.bumpChannel)
+    await channel.sendSlash('302050872383242240', 'bump')
+    console.count('Bumped!')
+}
 
 (function loop() {
-  // send bump message every 2-3 hours, to prevent detection.
-  var randomNum = Math.round(Math.random() * (9000000 - 7200000  + 1)) + 7200000;
-  setTimeout(function() {
-    bump();
-    loop();  
-  }, randomNum);
-}());
+    // send bump message every 2-3 hours, to prevent detection.
+    var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000
+    setTimeout(function () {
+        bump()
+        loop()
+    }, randomNum)
+}())
 
-client.login(config.token);
+client.login(config.token)
