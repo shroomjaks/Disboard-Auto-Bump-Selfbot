7094447587ed9f88dd80636142964263e5d00ed6
Wendel Hammes <wendelhammes@gmail.com>
2023-07-17 14:35:02 -0300

    Add dotenv
    added new library to be used in hosting safely and effectively

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const { Client } = require('discord.js-selfbot-v13')
+const client = new Client()
+
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+
+    const channel = await client.channels.fetch(process.env.bumpChannel)
+    
+    async function bump() {
+        await channel.sendSlash('302050872383242240', 'bump')
+        console.count('Bumped!')
+    }
+
+    function loop() {
+        // send bump message every 2-3 hours, to prevent detection.
+        var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000
+        setTimeout(function () {
+            bump()
+            loop()
+        }, randomNum)
+    }
+    
+    bump()
+    loop()
+})
+
+client.login(process.env.token)
